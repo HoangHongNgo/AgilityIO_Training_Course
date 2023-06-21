5c3aee927c03cdbcd482ad94609e96c0c2d168c4
HoangHongNgo <honghoangngo@gmail.com>
2023-06-21 09:34:13 +0700

    using main tag && header fixed when scroll && adjust css files arrangement

--- practice_one/src/js/scroll.js
+++ practice_one/src/js/scroll.js
@@ -1,13 +1,16 @@
 window.addEventListener("scroll", function() {
     var element = document.getElementsByClassName("header")[0];
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    var targetScroll = 500;
+    var targetScroll = 700;
   
     if (scrollTop > targetScroll) {
       element.style.background = "var(--bg-gradient-color)"; // Change to the desired background color
+      element.style.position = "fixed";
+      element.style.top = "0px";
     } else {
       element.style.backgroundColor = "rgba(0, 0, 0, 0)";
       element.style.background = ""; // Revert to the initial background color
+      element.style.position = "relative";
     }
   });
   
